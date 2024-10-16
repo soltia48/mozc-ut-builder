e052aaa445fd69f992249741e7496c35c1563679
soltia48 <soltia48@protonmail.com>
2024-10-17 01:38:23 +0900

    Add a apt install option

--- debian-based/install.debian.sh
+++ debian-based/install.debian.sh
@@ -9,5 +9,5 @@ if ! ls ./dist.debian/mozc-server_*.deb 1> /dev/null 2>&1; then
     bash ./build.debian.sh
 fi
 
-sudo apt -y install ./dist.debian/mozc-server_*.deb --reinstall --allow-change-held-packages \
+sudo apt -y install ./dist.debian/mozc-server_*.deb --reinstall --allow-downgrades --allow-change-held-packages \
     && sudo apt-mark hold mozc-server
--- debian-based/install.ubuntu.sh
+++ debian-based/install.ubuntu.sh
@@ -9,5 +9,5 @@ if ! ls ./dist.ubuntu/mozc-server_*.deb 1> /dev/null 2>&1; then
     bash ./build.ubuntu.sh
 fi
 
-sudo apt -y install ./dist.ubuntu/mozc-server_*.deb --reinstall --allow-change-held-packages \
+sudo apt -y install ./dist.ubuntu/mozc-server_*.deb --reinstall --allow-downgrades --allow-change-held-packages \
     && sudo apt-mark hold mozc-server
